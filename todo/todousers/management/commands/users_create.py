0b97039fa99497eee0cb92a1c825e4f3d271b08c
log-box <a.y.logvin@gmail.com>
2021-10-27 16:54:55 +0300

    lesson 1 DONE!

--- todo/todousers/management/commands/users_create.py
+++ todo/todousers/management/commands/users_create.py
@@ -11,6 +11,8 @@ class Command(BaseCommand):
 
         for i in range(1, 6):
             new_user = TodoUsers.objects.create_user(username=f'user{i}',
+                                                     first_name=f'user{i}_first_name',
+                                                     last_name=f'user{i}_last_name',
                                                      email=f'user{i}@mail.ru',
                                                      password=f'user{i}')
             new_user.save()
--- todo/todousers/views.py
+++ todo/todousers/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from todousers.models import TodoUsers
+from todousers.serializers import UsersModelSerializer
+
+
+class UsersModelViewSet(ModelViewSet):
+    queryset = TodoUsers.objects.all()
+    serializer_class = UsersModelSerializer
